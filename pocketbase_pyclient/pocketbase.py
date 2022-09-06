fc455d521f80734f8a98c67656692936fd41294a
J-wyt <546108612@qq.com>
2022-09-07 02:56:34 +0800

    Add auth models for admin and user

--- pocketbase_pyclient/pocketbase.py
+++ pocketbase_pyclient/pocketbase.py
@@ -11,24 +11,26 @@ class PocketBase:
     def __init__(self, url: str):
         from .auth import AuthService
         from .models import AuthStore
-
+        from .crud import CrudService
         self.url = url.strip('/')
         self.api = None
         self.collection = None
         self.auth_store: AuthStore = AuthStore()
         self._http = httpx.Client()
         self._auth_service = AuthService(self)
+        self._crud_service = CrudService(self)
 
     def http(self):
         return self._http
 
-    def auth_via_email(self, email: str, password: str):
+    def auth_via_email(self, email: str, password: str, admin: bool = False):
         """
         login in pocketbase with email and password
         :param email: email addr for login
         :param password: password for login
+        :param admin: login in as admin or not, default false
         """
-        self._auth_service.auth_via_email(email, password)
+        self._auth_service.auth_via_email(email, password, admin)
 
     def connect(self, collection: str):
         """
