f4914380bda1d294ac5e8ffe4cb8d9b5e7e59e3c
J-wyt <546108612@qq.com>
2022-09-25 23:12:17 +0800

    Fix login bug

--- pocketbase_pyclient/auth.py
+++ pocketbase_pyclient/auth.py
@@ -15,11 +15,12 @@ class AuthService(BaseService):
         super().__init__(pocketbase)
 
     def auth_via_email(self, email: str, password: str, admin: bool) -> AdminAuth | UserAuth:
-        target = "admin" if admin else "users"
+        target = "admins" if admin else "users"
+        prefix = 'Admin' if admin else 'User'
         clazz = AdminAuth if admin else UserAuth
         response = httpx.post(url=f"{self.client().url}/api/{target}/auth-via-email",
                               json={"email": email, "password": password})
         if response.is_success:
             json = response.json()
-            self.client().auth_store.save(json["token"])
+            self.client().auth_store.save(f'{prefix} {json["token"]}')
             return cattrs.structure(json, clazz)
