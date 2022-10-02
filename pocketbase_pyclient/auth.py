91e1e9309d49413340e17ace93df96f51a1301f0
j-wyt <546108612@qq.com>
2022-10-02 22:58:48 +0800

    Remove attrs

--- pocketbase_pyclient/auth.py
+++ pocketbase_pyclient/auth.py
@@ -2,7 +2,6 @@
 auth service
 """
 
-import cattrs
 import httpx
 
 from .crud import BaseService
@@ -17,10 +16,9 @@ class AuthService(BaseService):
     def auth_via_email(self, email: str, password: str, admin: bool) -> AdminAuth | UserAuth:
         target = "admins" if admin else "users"
         prefix = "Admin" if admin else "User"
-        clazz = AdminAuth if admin else UserAuth
+        # clazz = AdminAuth if admin else UserAuth
         response = httpx.post(url=f"{self.client().url}/api/{target}/auth-via-email",
                               json={"email": email, "password": password})
         if response.is_success:
             json = response.json()
             self.client().auth_store.save(f"{prefix} {json['token']}")
-            return cattrs.structure(json, clazz)
