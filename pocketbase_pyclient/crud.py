70db1d20fe2e1e33f0fcb855a9d71616041d2698
j-wyt <546108612@qq.com>
2022-10-03 20:54:45 +0800

    Add async http request

--- pocketbase_pyclient/crud.py
+++ pocketbase_pyclient/crud.py
@@ -1,7 +1,7 @@
 """
 crud service
 """
-import httpx
+from abc import ABC
 
 from .pocketbase import PocketBase
 
@@ -15,52 +15,38 @@ def _make_params(page, per_page, _sort, _filter):
     }
 
 
-class BaseService:
+class BaseService(ABC):
     def __init__(self, pocketbase: PocketBase):
         self._pocketbase = pocketbase
-        self.url = pocketbase.url
 
     def client(self):
         return self._pocketbase
 
-    def auth_header(self):
-        return self._pocketbase.auth_store.to_dict()
-
-    def request(self, url, method: str = "GET", params=None, json=None):
-        return httpx.request(url=url, method=method, params=params, json=json, headers=self.auth_header())
-
-    async def _async_request(self, url, method: str = "GET", params=None, json=None):
-        return await self._pocketbase.httpx.request(url=url, method=method, params=params, json=json,
-                                                    headers=self.auth_header())
+    def path(self, collection: str):
+        """Return service path"""
 
 
 class CrudService(BaseService):
     def __init__(self, pocketbase: PocketBase):
         super(CrudService, self).__init__(pocketbase)
 
-    def _api(self, collection: str):
-        return f"{self.url}/api/collections/{collection}/records"
+    def path(self, collection: str):
+        return f"/api/collections/{collection}/records"
 
-    def _id_api(self, collection: str, _id: str):
-        return f"{self._api(collection)}/{_id}"
+    def path_with_id(self, collection: str, _id: str):
+        return f"{self.path(collection)}/{_id}"
 
     def list(self, collection: str, page: int = 1, per_page: int = 30, _sort: str = "", _filter: str = ""):
-        return self.request(self._api(collection), params=_make_params(page, per_page, _sort, _filter)).json()
-
-    def list_items(self, collection: str, page: int = 1, per_page: int = 30, _sort: str = "", _filter: str = ""):
-        return self.list(collection, page, per_page, _sort, _filter)["items"]
+        return self.client().request(self.path(collection), params=_make_params(page, per_page, _sort, _filter))
 
     def view(self, collection, _id: str):
-        return self.request(self._id_api(collection, _id)).json()
+        return self.client().request(self.path_with_id(collection, _id))
 
     def create(self, collection: str, item):
-        return self.request(self._api(collection), "POST", json=item)
-
-    async def async_create(self, collection: str, item):
-        await self._async_request(self._api(collection), "POST", json=item)
+        return self.client().request(self.path(collection), "POST", json=item)
 
     def update(self, collection: str, _id: str, item):
-        return self.request(self._id_api(collection, _id), "PATCH", json=item)
+        return self.client().request(self.path_with_id(collection, _id), "PATCH", json=item)
 
     def delete(self, collection: str, _id: str):
-        return self.request(self._id_api(collection, _id), "DELETE")
+        return self.client().request(self.path_with_id(collection, _id), "DELETE")
--- pocketbase_pyclient/pocketbase.py
+++ pocketbase_pyclient/pocketbase.py
@@ -1,7 +1,23 @@
 """
 PocketBase client
 """
+from typing import Any
+
 import httpx
+
+
+class ClientException(Exception):
+    path: str = ""
+    status: int = 0
+    data: dict = {}
+    origin: Any | None = None
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args)
+        self.path = kwargs.get("path", "")
+        self.status = kwargs.get("status", 0)
+        self.data = kwargs.get("data", {})
+        self.origin = kwargs.get("origin", None)
 
 
 class PocketBase:
@@ -23,3 +39,36 @@ class PocketBase:
         :param admin: login in as admin or not, default false
         """
         self._auth.auth_via_email(email, password, admin)
+
+    async def request(
+            self,
+            path: str,
+            method: str = "GET",
+            headers: dict = None,
+            params: dict = None,
+            json: dict = None
+    ) -> Any:
+        headers.update(self.auth_store.to_dict())
+        try:
+            response = await self.httpx.request(
+                url=self.url + path,
+                method=method,
+                headers=headers,
+                params=params,
+                json=json
+            )
+        except Exception as e:
+            raise ClientException("Build request fail", origin=e)
+        try:
+            data = response.json()
+        except Exception as e1:
+            _ = e1
+            data = None
+        if response.status_code >= 400:
+            raise ClientException(
+                "Request fail",
+                url=response.url,
+                status=response.status_code,
+                data=data,
+            )
+        return data
